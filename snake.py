ec4704e9158a685d7febe257fef1bcfe5063d1a4
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-11-02 20:47:36 -0300

    implemenação da classe cube

--- snake.py
+++ snake.py
@@ -3,3 +3,15 @@ import random
 import pygame
 import tkinter as tk
 from tkinter import messagebox
+
+class Cube(object):
+    rows = 0
+    w = 0
+    def __init__(self,start,dirnx=1, dirny=0,color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
